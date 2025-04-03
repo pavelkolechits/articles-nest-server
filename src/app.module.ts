5687e331a8248739f746b8dd41df4df003fe7daa
pavelkolechits <pavelkolechits@gmail.com>
2025-04-03 18:04:14 +0300

    init commit, connect db

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+    controllers: [],
+    providers: [],
+    imports: [
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: 'byblik1889',
+            database: 'articles-project',
+            models: [],
+            autoLoadModels: true
+        }),
+    ],
+})
+export class AppModule { }
